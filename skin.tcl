5a78b48a4ac0ded4d9f6aecae3a8d6a292f523d3
Mimoja <git@mimoja.de>
2021-04-09 21:59:24 +0200

    Dont enable debugging
    Signed-off-by: Mimoja <git@mimoja.de>

--- skin.tcl
+++ skin.tcl
@@ -2,8 +2,8 @@ package require de1plus 1.0
 
 source "[homedir]/skins/default/standard_includes.tcl"
 
-set ::skindebug 1
-set ::debugging 1
+set ::skindebug 0
+set ::debugging 0
 set ::history_to_restore_after_cleanup {}
 
 namespace eval ::skin::mimojacafe::graph {}
